c5c0c15228c1887cf0c0d441dd0b94da8360601d
Prabhat Bhatia <prabhat@Prabhats-MacBook-Pro.local>
2026-02-02 11:20:39 +0530

    feat: something new

--- JS Lectures Files/11. Callbacks.js
+++ JS Lectures Files/11. Callbacks.js
@@ -39,3 +39,18 @@
 // When we have multiple nested callbacks, it can lead to code that is hard to read and maintain, 
 // often referred to as "callback hell" or "pyramid of doom".
 
+console.log("Starting homework...");
+
+setTimeout(() => {
+    console.log("Homework done!");
+    console.log("Starting dinner...");
+    
+    setTimeout(() => {
+        console.log("Dinner done!");
+        console.log("Getting ready to go out...");
+
+        setTimeout(() => {
+            console.log("Going to the playground!");
+        }, 1000); // after dinner
+    }, 1500); // dinner time
+}, 2000); // homework time
